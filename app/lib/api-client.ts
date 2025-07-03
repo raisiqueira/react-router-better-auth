66e2d2d7675a5a14e8b0c5539a0ec0a3914203ca
Rai Siqueira <rai93siqueira@gmail.com>
2025-07-02 21:24:54 -0300

    chore: update api-client

--- app/lib/api-client.ts
+++ app/lib/api-client.ts
@@ -1,8 +1,11 @@
 import type { AppRouter } from '~/server'
 import { hc } from 'hono/client'
 
-export const apiClient = hc<AppRouter>('http://localhost:5173', {
-  init: {
-    credentials: 'include',
-  },
+export const apiClient = hc<AppRouter>('http://localhost:5173/', {
+  fetch: ((input, init) => {
+    return fetch(input, {
+      ...init,
+      credentials: 'include',
+    })
+  }) satisfies typeof fetch,
 }).api
