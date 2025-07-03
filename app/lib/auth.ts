8391c1a4a026418e56cd4fce4617826f80f10d56
Rai Siqueira <rai93siqueira@gmail.com>
2025-07-02 21:04:57 -0300

    chore: add baseURL and trustedOrigins on auth.ts

--- app/lib/auth.ts
+++ app/lib/auth.ts
@@ -3,6 +3,8 @@ import { openAPI } from 'better-auth/plugins'
 import Database from 'better-sqlite3'
 
 export const auth = betterAuth({
+  baseURL: 'http://localhost:5173',
+  trustedOrigins: ['http://localhost:5173'],
   database: new Database('./sqlite.db'),
   emailAndPassword: {
     enabled: true,
