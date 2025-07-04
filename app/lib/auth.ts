f7a155efee93224701a3386826b9a61233429b73
Rai Siqueira <rai93siqueira@gmail.com>
2025-07-04 11:55:09 -0300

    chore: update session settings

--- app/lib/auth.ts
+++ app/lib/auth.ts
@@ -5,6 +5,10 @@ import Database from 'better-sqlite3'
 export const auth = betterAuth({
   baseURL: 'http://localhost:5173',
   trustedOrigins: ['http://localhost:5173'],
+  session: {
+    expiresIn: 60 * 60 * 24 * 30, // 30 days
+    freshAge: 0,
+  },
   database: new Database('./sqlite.db'),
   emailAndPassword: {
     enabled: true,
