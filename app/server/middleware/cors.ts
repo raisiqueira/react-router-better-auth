ed5b62cdd50f012817bcf71b26b07845aebcc1fa
Rai Siqueira <rai93siqueira@gmail.com>
2025-07-04 11:46:29 -0300

    chore: update cors middleware

--- app/server/middleware/cors.ts
+++ app/server/middleware/cors.ts
@@ -2,9 +2,9 @@ import { cors } from 'hono/cors'
 
 export const corsMiddleware = cors({
   origin: 'http://localhost:5173',
-  allowHeaders: ['Content-Type', 'Authorization'],
-  allowMethods: ['POST', 'GET', 'OPTIONS'],
-  exposeHeaders: ['Content-Length'],
+  allowHeaders: ['Content-Type', 'Authorization', 'Cookie'],
+  allowMethods: ['POST', 'GET', 'OPTIONS', 'PUT', 'DELETE'],
+  exposeHeaders: ['Content-Length', 'Set-Cookie'],
   maxAge: 600,
   credentials: true,
 })
